6a1766a4a2c8126fc634a422b16cb0bfe501b071
Pau Diaz <user@MacBook-Pro-de-Pau.local>
2024-11-02 11:25:37 +0100

    First project commit(WS1.0)

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.getElementById('formRegistro').addEventListener('submit', function(event){
+    event.preventDefault();
+    // Lógica para registrar al usuario
+    alert('Registro completado');
+});
+
+document.getElementById('formLogin').addEventListener('submit', function(event){
+    event.preventDefault();
+    // Lógica para iniciar sesión
+    alert('Inicio de sesión exitoso');
+});
+
+document.getElementById('formSubida').addEventListener('submit', function(event){
+    event.preventDefault();
+    // Lógica para subir el archivo
+    alert('Archivo subido');
+});
+
+
